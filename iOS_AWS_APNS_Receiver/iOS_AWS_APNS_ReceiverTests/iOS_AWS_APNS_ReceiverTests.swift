f6762cd43250a7386f733b9f6819699000bc31b5
roanacla <roanacla@me.com>
2020-10-04 17:38:34 -0700

    Add .gitignore file

---
+++ iOS_AWS_APNS_Receiver/iOS_AWS_APNS_ReceiverTests/iOS_AWS_APNS_ReceiverTests.swift
@@ -0,0 +1,33 @@
+//
+//  IOS_AWS_APNs_ReceiverTests.swift
+//  IOS_AWS_APNs_ReceiverTests
+//
+//  Created by Roger Navarro on 10/4/20.
+//
+
+import XCTest
+@testable import IOS_AWS_APNs_Receiver
+
+class IOS_AWS_APNs_ReceiverTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
